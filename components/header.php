2ddf4f75ab1a38f9504fcbaa709f5c6e4a4345c2
Ri4Chaard <polovnikovyaroslav@gmail.com>
2024-11-10 21:24:50 +0300

    Nav menu links styles updated

--- components/header.php
+++ components/header.php
@@ -7,10 +7,11 @@
               <?php
                 $args = array(
                     'theme_location' => 'top-menu',
-                    'before' => '<span class="absolute inset-0 h-[.5px]  top-1/2 transform scale-x-0 group-hover:scale-x-100 transition-all duration-500 origin-left bg-white"></span>',
-                    'after' => '',
+                    'link_before' => '<span class="absolute m-5 inset-0 h-[.5px] -top-1/3 transform scale-x-0 group-hover:scale-x-100 transition-all duration-500 origin-left bg-white"></span>',
+                    'link_after' => '',
                     'items_wrap' => '<ul id="%1$s" class="flex mr-12">%3$s</ul>',
-                    'add_li_class'  => 'p-5 group relative',
+                    'list_item_class'  => 'nav-item relative',
+                    'link_class'  => 'group p-5 ',
                 );
 
                 wp_nav_menu($args);
